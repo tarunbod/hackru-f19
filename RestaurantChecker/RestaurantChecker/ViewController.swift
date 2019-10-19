b5a616f4a98a740347077f623c00735ac5d7ccb7
Tarun Boddupalli <tarunbod@gmail.com>
2019-10-19 16:29:17 -0400

    Use storyboards instead

---
+++ RestaurantChecker/RestaurantChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantChecker
+//
+//  Created by Tarun Boddupalli on 10/19/19.
+//  Copyright © 2019 Tarun Boddupalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
