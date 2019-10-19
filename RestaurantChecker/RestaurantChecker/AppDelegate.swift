13c5cec369a0480636d85e533473463cb7a58fdd
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-10-19 15:17:02 -0400

    Swift Xcode Project

---
+++ RestaurantChecker/RestaurantChecker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RestaurantChecker
+//
+//  Created by Bharddwaj Vemulapalli on 10/19/19.
+//  Copyright © 2019 Bharddwaj Vemulapalli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
