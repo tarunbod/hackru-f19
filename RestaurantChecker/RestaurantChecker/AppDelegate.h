4f8c6db12322545660feadcb85ea056dddf66e00
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-10-19 14:53:29 -0400

    XCode Project

---
+++ RestaurantChecker/RestaurantChecker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RestaurantChecker
+//
+//  Created by Bharddwaj Vemulapalli on 10/19/19.
+//  Copyright © 2019 Bharddwaj Vemulapalli. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
